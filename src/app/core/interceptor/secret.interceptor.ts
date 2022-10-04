1877da066bdc061766741d4d7c70ec3bd770d377
Tran-Minh-Quang <101007740+Tran-Minh-Quang@users.noreply.github.com>
2022-10-04 17:53:00 +0700

    update

---
+++ src/app/core/interceptor/secret.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+    HttpErrorResponse,
+    HttpEvent,
+    HttpHandler,
+    HttpHeaders,
+    HttpInterceptor,
+    HttpRequest,
+  } from '@angular/common/http';
+  import { Injectable } from '@angular/core';
+  import {
+    BehaviorSubject,
+    catchError,
+    filter,
+    Observable,
+    switchMap,
+    take,
+    throwError,
+  } from 'rxjs';
+  
+  @Injectable()
+  export class SecretInterceptor implements HttpInterceptor {
+    intercept(
+      request: HttpRequest<any>,
+      next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+      const token = localStorage.getItem('access_token');
+      if(token) {
+        const AuthRequest = request.clone({setHeaders: {
+            'Authorization': `Bearer ${token}`,
+          }});
+          return next.handle(AuthRequest);
+      } else {
+        return next.handle(request)
+      }
+    }
+  }
+  
