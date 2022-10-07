c13156271df2dbb32d7a6adef21ef1f90399e998
Tran-Minh-Quang <101007740+Tran-Minh-Quang@users.noreply.github.com>
2022-10-07 17:48:56 +0700

    update

--- src/app/features/orders-mgm/orders-mgm.module.ts
+++ src/app/features/orders-mgm/orders-mgm.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from 'src/app/core/shared/shared.module';
 import { OrdersMgmRoutingModule } from './orders-mgm-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MenuCollapseComponent } from 'src/app/core/component/common/menu-collapse/menu-collapse.component';
+import { CreatePurchaseOrderComponent } from './create-purchase-order/create-purchase-order.component';
 
 @NgModule({
-    declarations: [SidebarComponent, OrdersMgmComponent, MenuCollapseComponent],
+    declarations: [SidebarComponent, OrdersMgmComponent, MenuCollapseComponent, CreatePurchaseOrderComponent],
     imports: [CommonModule, SharedModule, OrdersMgmRoutingModule, RouterModule],
 })
 export class OrdersMgmModule {}
