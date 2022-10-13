a6e10c68e4064d1226b55737e6350125a0366480
DESKTOP-KJNF2QE\Admin <vantu2102@gmail.com>
2022-10-13 17:52:03 +0700

    update

---
+++ src/app/features/route/route-tree-view/route-tree-view.component.ts
@@ -0,0 +1,81 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ITreeOptions } from '@circlon/angular-tree-component';
+
+@Component({
+  selector: 'app-route-tree-view',
+  templateUrl: './route-tree-view.component.html',
+  styleUrls: ['./route-tree-view.component.scss']
+})
+export class RouteTreeViewComponent implements OnInit {
+  @Input() nodes: any[] = [
+    {
+      id: 'all',
+      name: 'Tất cả',
+      children: [
+        {
+          id: 2, name: 'child1',
+          children: [
+            { id: 4, name: 'child1' },
+            {
+              id: 5, name: 'child2',
+              children: [
+                {
+                  id: 8, name: 'child1',
+                  children: [
+                    { id: 10, name: 'child1' },
+                    { id: 11, name: 'child2' }
+                  ]
+                },
+                {
+                  id: 9, name: 'child2',
+                  children: [
+                    { id: 12, name: 'child1' },
+                    { id: 13, name: 'child2' }
+                  ]
+                }
+              ]
+            },
+            {
+              id: 14, name: 'child2',
+              children: [
+                {
+                  id: 15, name: 'child1',
+                  children: [
+                    { id: 16, name: 'child1' },
+                    { id: 17, name: 'child2' }
+                  ]
+                },
+                {
+                  id: 19, name: 'child2',
+                  children: [
+                    { id: 18, name: 'child1' },
+                    { id: 20, name: 'child2' }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 3, name: 'child2',
+          children: [
+            { id: 6, name: 'child1' },
+            { id: 7, name: 'child2' }
+          ]
+        }
+      ]
+    }
+  ];
+  options: ITreeOptions = {
+    useCheckbox: true,
+  };
+  settings: any = {}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  Config(e: any) { 
+    console.log(e);
+    
+  }
+}
---
+++ src/app/features/route/route.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouteComponent } from './route.component';
+import { SharedModule } from 'src/app/core/shared/shared.module';
+import { RouterModule, Routes } from '@angular/router';
+import { RouteTreeViewComponent } from './route-tree-view/route-tree-view.component';
+
+const routes: Routes = [
+  {
+      path: '',
+      component: RouteComponent,
+  },
+];
+
+@NgModule({
+  declarations: [
+    RouteComponent,
+    RouteTreeViewComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class RouteModule { }
