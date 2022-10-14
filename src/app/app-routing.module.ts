e327bac2c99455a90895fe1dd3b56b2563be6423
Bui Duy Manh <manh.bd206155@sis.hust.edu.vn>
2022-10-14 13:24:37 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,16 @@ const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./core/layouts/main/main.module').then((m) => m.MainModule),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
     },
     {
         path: 'auth',
         loadChildren: () => import('./core/layouts/auth/auth.module').then((m) => m.AuthModule),
     },
-    { path: 'returns', loadChildren: () => import('./features/returns/returns.module').then(m => m.ReturnsModule) }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
