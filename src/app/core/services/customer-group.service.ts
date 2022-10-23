899e998bbc69ff116cb01b94befe34d4fcc00365
Duy Le Quang <duyle7581@gmail.com>
2022-10-23 17:28:26 +0700

    ud

--- src/app/core/services/customer-group.service.ts
+++ src/app/core/services/customer-group.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, pipe } from 'rxjs';
-import { customer_group_url } from '../const/url';
+import { customer_group_url, gateway_url } from '../const/url';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +9,18 @@ import { customer_group_url } from '../const/url';
 export class CustomerGroupService {
 
   constructor(private http: HttpClient) { }
-
+  url = gateway_url + "/SearchEmployeeInGroup"
   get_all(): Observable<any> {
     return this.http.get(customer_group_url + '/getall')
       .pipe(
         map((response: any) => response)
       )
+  }
+
+  SearchEmployeeInGroup(id:any, page:any, pageSize:any):Observable<any>{
+    return this.http.get(this.url + "?GroupId=" + id + "&page=" + page + "&pagesize="+ pageSize)
+    .pipe(
+      map(res => res)
+    )
   }
 }
--- src/app/features/route/infor-router/infor-router.component.ts
+++ src/app/features/route/infor-router/infor-router.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { CustomerGroupService } from 'src/app/core/services/customer-group.service';
 import { DataService } from 'src/app/core/services/data.service';
 import { RouteService } from 'src/app/core/services/route.service';
 import { RootInfoRoute } from '../models/infor-router';
@@ -17,41 +18,85 @@ export class InforRouterComponent implements OnInit {
     public datePipe: DatePipe,
     private _routeSer: RouteService,
     private fb: FormBuilder,
+    private customerGroupSer: CustomerGroupService
   ) { }
 
   @Input() status: any;
   @Input() idRoute: any;
-
+  formatDate:any;
   showGroup:boolean = false;
   RootInfoRouteDetail :RootInfoRoute =  new RootInfoRoute;
-
-  formInforRoute =  this.fb.group({
+  groupName:any;
+  formInforRouteUpdate =  this.fb.group({
     routeCode: [''],
     routeName: [''],
     employeeId: [''],
-    employee: [''],
+    // employee: [''],
     unitTreeGroupId: [''],
-    unitTreeGroup: [''],
+    // unitTreeGroup: [''],
     routeDate: [''],
     startedDate: [''],
     status: true,
-    routeCustomer: [''],
+    // routeCustomer: [''],
     id: [''],
-    createdBy: [''],
-    createdDate: [''],
-    lastModifiedBy: [''],
-    lastModifiedDate: ['']
-  })
+    // createdBy: [''],
+    // createdDate: [''],
+    // lastModifiedBy: [null],
+    // lastModifiedDate: [null]
+  });
+
+
+  EmployeeInGroup:any;
   ngOnInit(): void {
     console.log(this.idRoute);
     this.getRouteDetail();
+    this.dataService.employee.subscribe({
+      next: data => {
+        console.log(data);
+        console.log('OK');
+        this.formInforRouteUpdate.patchValue({
+          startedDate: new Date(this.formatDate).toISOString(),
+          id: this.RootInfoRouteDetail.id,
+          unitTreeGroupId: this.RootInfoRouteDetail.unitTreeGroupId,
+          employeeId: this.RootInfoRouteDetail.employeeId,
+
+        });
+        console.log(this.formInforRouteUpdate.value);
+        this._routeSer.UpdateRoute(this.formInforRouteUpdate.value).subscribe({
+          next: data => {
+            console.log(data);
+            this.getRouteDetail();
+          }
+        })
+      }
+    })
 
   }
   getRouteDetail(){
     this._routeSer.GetRouteById(this.idRoute).subscribe({
       next: data => {
         console.log(data);
-        this.RootInfoRouteDetail = data as RootInfoRoute
+        this.RootInfoRouteDetail = data as RootInfoRoute;
+        this.formatDate = data.startedDate.split("T")[0];
+        console.log(this.formatDate);
+        this.groupName = data.unitTreeGroup.name;
+        this.SearchEmployeeInGroup(data.unitTreeGroup.id)
+      }
+    })
+  };
+  getIdName(event:any){
+    console.log(event);
+    this.groupName = event.name;
+    this.showGroup = !this.showGroup;
+    this.SearchEmployeeInGroup(event.id)
+  }
+
+  SearchEmployeeInGroup(id:any){
+    this.customerGroupSer.SearchEmployeeInGroup(id, "1", "10000").subscribe({
+      next: data => {
+        console.log(data);
+        this.EmployeeInGroup = data.data;
+
       }
     })
   }
