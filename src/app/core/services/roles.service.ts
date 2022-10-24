6353ee1d4b0f7e3b8454be97b57cf4c27801ca81
Bui Duy Manh <manh.bd206155@sis.hust.edu.vn>
2022-10-24 17:38:18 +0700

    no message

--- src/app/core/services/roles.service.ts
+++ src/app/core/services/roles.service.ts
@@ -7,10 +7,12 @@ import { AuthService } from './auth.service';
 })
 export class RolesService {
     private currentRoleList: string[] = [];
+    private currentRole: string;
     constructor(private authService: AuthService) {}
 
     fetchRoles() {
         const roleString = localStorage.getItem('role') || null;
+        const mainRole = localStorage.getItem('roleMain') || null;
         if (roleString) {
             //convert string separate by comma to array
             const array: string[] = roleString.split(',');
@@ -18,9 +20,18 @@ export class RolesService {
         } else {
             this.authService.logout();
         }
+        if (mainRole) {
+            //convert string separate by comma to array
+            this.currentRole = mainRole.toUpperCase();
+        } else {
+            this.authService.logout();
+        }
     }
 
     requiredRoles(role: string) {
         return of(this.currentRoleList.includes(role.toUpperCase()));
+    }
+    onlyRole(role: string) {
+        return of(this.currentRole === role.toUpperCase());
     }
 }
