35f86286b4d30e39c8478f1f62df69417ca166b3
Tran-Minh-Quang <101007740+Tran-Minh-Quang@users.noreply.github.com>
2022-10-24 15:59:53 +0700

    update

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -5,37 +5,44 @@ import { api_url } from '../const/url';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
+    constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { }
+    Authenticate(body: any): Observable<any> {
+        return this.http.post(api_url + '/authenticate', body).pipe(map((response: any) => response));
+    }
 
-  Authenticate(body: any): Observable<any> {
-    return this.http.post(api_url + '/authenticate', body)
-      .pipe(
-        map((response: any) => response)
-      )
-  }
+    setToken(token: string) {
+        localStorage.setItem('access_token', token);
+    }
 
-  setToken(token: string) {
-    localStorage.setItem("access_token", token)
-  }
+    setRoles(role: string) {
+        const roleList = role.split(',');
+        let roleMain: any;
+        if(roleList.includes("MEMBER")) {
+          roleMain = "member"
+        }
+        if(roleList.includes("MEMBER") && roleList.includes("ACCOUNTANT")) {
+          roleMain = "accountant"
+        }
+        if(roleList.includes("MEMBER") && roleList.includes("ACCOUNTANT")&& roleList.includes("MANAGER")) {
+          roleMain = "manager"
+        }
+        if(roleList.includes("MEMBER") && roleList.includes("ACCOUNTANT")&& roleList.includes("MANAGER") && roleList.includes("SALE_ADMIN")) {
+          roleMain = "admin"
+        }
+        localStorage.setItem('roleMain', roleMain);
+        localStorage.setItem('role', role);
+    }
 
-  setRoles(role: string) {
-    localStorage.setItem("role", role);
-  }
+    isLoggednIn() {
+        return localStorage.getItem('access_token') != null;
+    }
 
-  isLoggednIn() {
-    return localStorage.getItem('access_token') != null
-  }
-
-  logout() {
-    localStorage.clear();
-    this.router.navigate(['/auth/login']);
-  }
-
+    logout() {
+        localStorage.clear();
+        this.router.navigate(['/auth/login']);
+    }
 }
