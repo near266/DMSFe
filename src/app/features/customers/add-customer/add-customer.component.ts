05ecf56b109f671c32f235f23a68c36148e3bd95
Đinh Ngọc Đức <dnduc12bhla@gmail.com>
2022-10-26 17:18:33 +0700

    change select to input province

--- src/app/features/customers/add-customer/add-customer.component.ts
+++ src/app/features/customers/add-customer/add-customer.component.ts
@@ -45,6 +45,8 @@ export interface IBody {
 })
 export class AddCustomerComponent implements OnInit, AfterViewInit {
 
+  loading = true;
+  tabs = 'Information';
   customerGroup: CustomerGroup[] = [];
   customerType: CustomerType[] = [];
   channel: Channel[] = [];
@@ -52,8 +54,21 @@ export class AddCustomerComponent implements OnInit, AfterViewInit {
   listProvinces: any[] = [];
   listDistricts: any[] = [];
   listWards: any[] = [];
-
+  showArea = false;
+  showProvince = false;
+  showDistrict = false;
+  showWard = false;
   isChoseUpdated = false;
+  areaName = '';
+  provinceName = '';
+  districtName = '';
+  wardName = '';
+  areaTemp: Area[] = [];
+  provinceTemp: any[]= [];
+  districtTemp: any[]= [];
+  wardTemp: any[]= [];
+
+
 
   form: FormGroup;
   constructor(
@@ -69,20 +84,43 @@ export class AddCustomerComponent implements OnInit, AfterViewInit {
     @Inject(MAT_DIALOG_DATA) public data: string,
     ) { }
   ngAfterViewInit(): void {
+    let count = 0;
     this.customerGroupService.get_all().subscribe(data => {
       this.customerGroup = data as CustomerGroup[];
+      count++;
+      if(count == 5) {
+        this.loading = false;
+      }
     });
     this.customerTypeService.get_all().subscribe(data => {
       this.customerType = data;
+      count++;
+      if(count == 5) {
+        this.loading = false;
+      }
     });
     this.channelService.get_all().subscribe( data => {
       this.channel = data;
+      count++;
+      if(count == 5) {
+        this.loading = false;
+      }
     });
     this.areaService.get_all().subscribe( data => {
       this.area = data;
+      this.areaTemp = this.area;
+      count++;
+      if(count == 5) {
+        this.loading = false;
+      }
     });
     this.provincesService.getListProvinces().subscribe(data => {
       this.listProvinces = data;
+      this.provinceTemp = this.listProvinces;
+      count++;
+      if(count == 5) {
+        this.loading = false;
+      }
     });
   }
 
@@ -115,11 +153,14 @@ export class AddCustomerComponent implements OnInit, AfterViewInit {
 
   getDistrict(event: any) {
     this.form.controls['district'].setValue('');
+    this.districtName = '';
     this.form.controls['ward'].setValue('');
+    this.wardName = '';
     this.listProvinces.forEach(data => {
       if(data.name == event) {
         this.provincesService.getDistrictsListByID(data.code).subscribe(res => {
           this.listDistricts = res.districts;
+          this.districtTemp = this.listDistricts;
         });
       }
     });
@@ -127,10 +168,12 @@ export class AddCustomerComponent implements OnInit, AfterViewInit {
 
   getWard(event: any) {
     this.form.controls['ward'].setValue('');
+    this.wardName = '';
     this.listDistricts.forEach(data => {
       if(data.name == event) {
         this.provincesService.getWardsListByID(data.code).subscribe(res => {
           this.listWards = res.wards;
+          this.wardTemp = this.listWards;
         });
       }
     });
@@ -141,6 +184,7 @@ export class AddCustomerComponent implements OnInit, AfterViewInit {
   }
 
   add() {
+    this.loading = true;
     let body = {
       // id: this.form.controls['customerGroupId'].value,
       // customerCode: this.form.controls['customerCode'].value,
@@ -173,12 +217,127 @@ export class AddCustomerComponent implements OnInit, AfterViewInit {
     }
 
     this.customerService.add(body).subscribe(data => {
+      this.loading = false;
       this.snackbar.openSnackbar('Thêm khách hàng thành công', 2000, 'Đóng', 'center', 'bottom', true);
       this.dialogRef.close({event: true});
     }, (error) => {
+      this.loading = false;
       this.snackbar.openSnackbar('Thêm khách hàng không thành công, vui lòng kiểm tra thông tin đã nhập', 2000, 'Đóng', 'center', 'bottom', true);
     });
 
+  }
+
+  selectArea(value: any) {
+    this.showArea = false;
+    this.areaName = value.areaName;
+    this.form.controls['areaId'].setValue(value.id);
+  }
+  searchArea() {
+    if(this.areaName != '') {
+      this.areaTemp = [];
+      this.area.forEach((element) => {
+        if(this.removeVietnameseTones(element.areaName.toLowerCase()).includes(this.removeVietnameseTones(this.areaName.toLowerCase()))) {
+          this.areaTemp.push(element);
+        }
+      });
+    } else {
+      this.areaTemp = this.area;
+    }
+
+  }
+
+  selectProvince(value: any) {
+    this.showProvince = false;
+    this.provinceName = value.name;
+    this.form.controls['province'].setValue(value.name);
+    this.getDistrict(value.name);
+  }
+
+  searchProvince() {
+    if(this.provinceName != '') {
+      this.provinceTemp = [];
+      this.listProvinces.forEach((element) => {
+        if(this.removeVietnameseTones(element.name.toLowerCase()).includes(this.removeVietnameseTones(this.provinceName.toLowerCase()))) {
+          this.provinceTemp.push(element);
+        }
+      });
+    } else {
+      this.provinceTemp = this.listProvinces;
+    }
+  }
+
+  selectDistrict(value: any) {
+    this.showDistrict = false;
+    this.districtName = value.name;
+    this.form.controls['district'].setValue(value.name);
+    this.getWard(value.name);
+  }
+
+  searchDistrict() {
+    if(this.districtName != '') {
+      this.districtTemp = [];
+      this.listProvinces.forEach((element) => {
+        if(this.removeVietnameseTones(element.name.toLowerCase()).includes(this.removeVietnameseTones(this.districtName.toLowerCase()))) {
+          this.districtTemp.push(element);
+        }
+      });
+    } else {
+      this.wardTemp = this.listWards;
+    }
+  }
+
+  selectWard(value: any) {
+    this.showWard = false;
+    this.wardName = value.name;
+    this.form.controls['ward'].setValue(value.name);
+  }
+
+  searchWard() {
+    if(this.wardName != '') {
+      this.wardTemp = [];
+      this.listWards.forEach((element) => {
+        if(this.removeVietnameseTones(element.name.toLowerCase()).includes(this.removeVietnameseTones(this.wardName.toLowerCase()))) {
+          this.wardTemp.push(element);
+        }
+      });
+    } else {
+      this.wardTemp = this.listWards;
+    }
+  }
+
+  removeVietnameseTones(str: any) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+    str = str.replace(/đ/g, 'd');
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, 'A');
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, 'E');
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, 'I');
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, 'O');
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, 'U');
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, 'Y');
+    str = str.replace(/Đ/g, 'D');
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, '');
+    str = str.replace(/\u02C6|\u0306|\u031B/g, '');
+    str = str.replace(/ + /g, ' ');
+    str = str.trim();
+    str = str.replace(
+      /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+      ' '
+    );
+    return str;
+  }
+
+  unfocus() {
+    if(this.showArea == true || this.showProvince == true || this.showDistrict == true || this.showWard == true) {
+      this.showArea = false;
+      this.showProvince = false;
+      this.showDistrict = false;
+      this.showWard = false;
+    }
   }
 
 }
