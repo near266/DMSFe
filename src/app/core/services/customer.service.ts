fa4eda9defa6c77b85ae6865b5a385c124d55903
Duy Le Quang <duyle7581@gmail.com>
2022-11-04 18:58:37 +0700

    update preview

--- src/app/core/services/customer.service.ts
+++ src/app/core/services/customer.service.ts
@@ -72,8 +72,8 @@ export class CustomerService {
         map((response: any) => response)
       )
   }
-  SearchAllCustomerByCode(body: any): Observable<any> {
-    return this.http.post(customer_url + '/SearchAllCustomerByCode', body)
+  GetCustomerByCode(body: any): Observable<any> {
+    return this.http.post(customer_url + '/GetCustomerByCode', body)
       .pipe(
         map((response: any) => response)
       )
