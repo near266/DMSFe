2930ba6da544e0d353a9cebead5ec8b9f62c5627
Tran-Minh-Quang <101007740+Tran-Minh-Quang@users.noreply.github.com>
2022-11-28 12:22:10 +0700

    update

---
+++ src/app/features/visit-report/visit-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-report',
+  templateUrl: './visit-report.component.html',
+  styleUrls: ['./visit-report.component.scss']
+})
+export class VisitReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
