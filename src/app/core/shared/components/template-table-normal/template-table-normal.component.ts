b864780b19102e12c82a3818dd0fe3401e34ece3
Tran-Minh-Quang <101007740+Tran-Minh-Quang@users.noreply.github.com>
2022-12-26 15:24:43 +0700

    update

---
+++ src/app/core/shared/components/template-table-normal/template-table-normal.component.ts
@@ -0,0 +1,35 @@
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChanges,
+    ViewChild,
+} from '@angular/core';
+
+@Component({
+    selector: 'app-template-table-normal',
+    templateUrl: './template-table-normal.component.html',
+    styleUrls: ['./template-table-normal.component.scss'],
+})
+export class TemplateTableNormalComponent implements OnInit, AfterViewInit {
+    @ViewChild('header') header!: ElementRef;
+    @Input() headers: string[] = [];
+    @Input() rows: CRow[] = [];
+    @Input() colorHeader: string = '';
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {
+        this.header.nativeElement.style.backgroundColor = this.colorHeader;
+        this.header.nativeElement.style.color = 'white';
+    }
+}
+
+export class CRow {
+    listCol: string[];
+    colspan?: number;
+}
