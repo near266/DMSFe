9ff9b86b834e5e5a2827a2cd50ab295e41963d3a
Đinh Ngọc Đức <dnduc12bhla@gmail.com>
2023-01-05 15:47:47 +0700

    no message

--- src/app/features/timekeeping/component/pagination/pagination.component.ts
+++ src/app/features/timekeeping/component/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { PurchaseOrderService } from 'src/app/core/services/purchaseOrder.service';
 
 @Component({
@@ -8,43 +8,15 @@ import { PurchaseOrderService } from 'src/app/core/services/purchaseOrder.servic
 })
 export class PaginationComponent implements OnInit {
 
-  startIndex: number;
-  endIndex: number;
-  page: number = 1;
-  pageSize: number = 30;
-  total: number = 0;
-  constructor(private purchaseOrderService: PurchaseOrderService) {}
-  ngOnInit(): void {
-      this.purchaseOrderService.total.subscribe((data) => {
-          this.total = data;
-          this.setIndexToFirstPage();
-      });
-  }
-  setIndexToFirstPage() {
-      this.startIndex = 1;
-      this.endIndex = Math.min(this.startIndex + this.pageSize, this.total);
-  }
-  setIndexToSecondPageToDiffrentFirstPage(event: any) {
-      this.startIndex = (event - 1) * this.pageSize + event;
-      this.endIndex = Math.min(this.startIndex + this.pageSize, this.total);
-  }
+    @Output() pageCurrent$ = new EventEmitter<number>();
 
-  onTableDataChange(event: any) {
-      if (event === 1) {
-          this.setIndexToFirstPage();
-      } else {
-          this.setIndexToSecondPageToDiffrentFirstPage(event);
-      }
-      this.page = event;
-      this.purchaseOrderService.changePage(this.page);
-  }
+    page: number = 1;
+    constructor() {}
 
-  getStartAndEndIndex() {}
-
-  onTableSizeChange(event: any): void {
-      this.pageSize = event.target.value;
-      this.page = 1;
-      // this.fetchPosts();
-  }
+    ngOnInit(): void {}
+    onTableDataChange(event: any) {
+        this.page = event;
+        this.pageCurrent$.emit(this.page);
+    }
 
 }
