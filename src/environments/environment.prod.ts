38bded043d10687bf919b4cf2a4c2a8c72ee47c9
near266 <vuquangnam2662002@gmail.com>
2023-07-31 11:10:04 +0700

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,8 @@ export const environment = {
     // UPLOAD_URL: 'https://cdn-fitolabs.eztek.net',
 
     // API TEST
-    // api: 'https://fitolabs-api.eztek.net',
-    // ID_URL: 'https://fitolabs-id.eztek.net',
-    // API_URL: 'https://fitolabs-api.eztek.net',
-    // UPLOAD_URL: 'https://cdn-fitolabs.eztek.net',
+    api: 'https://fitolabs-api.eztek.net',
+    ID_URL: 'https://fitolabs-id.eztek.net',
+    API_URL: 'https://fitolabs-api.eztek.net',
+    UPLOAD_URL: 'https://cdn-fitolabs.eztek.net',
 };
