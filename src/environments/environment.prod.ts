ca47dda7d80555a3d7c94e5741a3b0d65404b2b5
near266 <vuquangnam2662002@gmail.com>
2023-08-07 01:01:53 +0700

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
     production: true,
 
    // API PRODUCT
-    api: 'https://dms-a.fitolabs.vn',
-    ID_URL: 'https://id.fitolabs.vn',
-    API_URL: 'https://dms-a.fitolabs.vn',
-    UPLOAD_URL: 'https://cdn-fitolabs.eztek.net',
+        api: 'https://dms-a.fitolabs.vn',
+        ID_URL: 'https://id.fitolabs.vn',
+        API_URL: 'https://dms-a.fitolabs.vn',
+        UPLOAD_URL: 'https://cdn-fitolabs.eztek.net',
 
     // API TEST
     // api: 'https://fitolabs-api.eztek.net',
